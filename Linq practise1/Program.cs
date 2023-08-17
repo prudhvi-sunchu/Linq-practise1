f7f237f067a1117f01bc9cd2371549c770689155
prudhvi <prudhvi@MSI>
2023-08-17 16:51:10 +0530

    Add project files.

---
+++ Linq practise1/Program.cs
@@ -0,0 +1,21 @@
+﻿//using System.Linq;
+//using System.Collections;
+//using System.Collections.Generic;
+
+//class Program
+//{
+//    static void Main(string[] args) 
+//    {
+//        int[] numbers = new int[] { 1, 2, 6, 7, 8, 9, 10, 3, 4, 5 };
+
+//        List<string> names = new List<string>() { "Sai","Ravi","Mahesh","Pavan","Nikhil"};
+
+//        var ms = numbers.Skip(3).ToArray();
+
+//        var mixeds = (from n in names select n).Skip(2).ToList();
+//      foreach (var num in ms)
+//        {
+//            Console.WriteLine(ms);
+//        }
+//    }
+//}
---
+++ Linq practise1/linq samples.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_practise1
+{
+    class Progam
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 1,2,3,4,5,6,7,8,9,10}; //Data source
+
+            var querySyntax = from obj in list
+                              where obj > 2       //Query
+                              select obj;
+
+            foreach (var item in querySyntax)
+            {
+                Console.WriteLine(item);       //Execution
+            }
+            Console.WriteLine("------------------------");
+
+            var methodSyntax = list.Where(obj => obj > 2);
+
+            foreach (var item in querySyntax)
+            {
+                Console.WriteLine(item);       //Execution
+            }
+            Console.WriteLine("------------------------");
+            var mixedSyntax = (from obj in list 
+                               select obj).Max();
+            Console.WriteLine("max value =" + mixedSyntax);
+
+            Console.ReadLine();
+        }
+    }
+}
